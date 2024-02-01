b23d5479c70df379181acf1eafa8fa64bad7d87d
aer-i <mateusz.kallus@zslt.klucznet.pl>
2024-02-01 19:35:16 +0100

    Added triangle and ImGui docking

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <vector>
+#include <array>
+#include <fstream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <quickmath.hpp>
 #include <imgui.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <backends/imgui_impl_glfw.h>
+
+constexpr char const* vert = R"(
+#version 460
+layout (location = 0) in vec3 a_pos;
+
+void main()
+{
+    gl_Position = vec4(a_pos, 1.0);
+}
+)";
+
+constexpr char const* frag = R"(
+#version 460
+layout(location = 0) out vec4 o_color;
+
+void main()
+{
+    o_color = vec4(1.0);
+}
+)";
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
@@ -53,7 +77,17 @@ auto static const program = []() noexcept -> int
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    // Disabled for now
+    //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;  
     ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 460");
@@ -63,6 +97,43 @@ auto static const program = []() noexcept -> int
     glfwGetWindowSize(window, &w, &h);
     glViewport(0, 0, w, h);
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);  
+
+    uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vert, NULL);
+    glCompileShader(vertexShader);
+
+    uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &frag, NULL);
+    glCompileShader(fragmentShader);
+
+    uint32_t shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+        -0.5f,  0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f 
+    }; 
+
+    uint32_t vertexBuffer, vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+    glBindVertexArray(vertexArray);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    glBindVertexArray(0);
 
     while(!glfwWindowShouldClose(window))
     {
@@ -88,15 +159,32 @@ auto static const program = []() noexcept -> int
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), ImGuiDockNodeFlags_PassthruCentralNode);
+
+
         ImGui::ShowDemoWindow();
-        
+
         #pragma endregion
         #pragma region Render commands
-        glClearColor(1.f, 1.f, 1.f, 1.f);
+        glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertexArray);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
         #pragma endregion
 
         glfwSwapBuffers(window);
---
+++ src/shaders/main.frag
@@ -0,0 +1,7 @@
+#version 460
+layout(location = 0) out vec4 o_color;
+
+void main()
+{
+    o_color = vec4(0.0);
+}
